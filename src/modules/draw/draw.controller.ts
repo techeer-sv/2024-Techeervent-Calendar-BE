a4fe02d12d78be84c7be0a5e751f7d93a24a18c4
dlwhsk0 <hana.dev.314@gmail.com>
2024-12-05 02:29:59 +0900

    feat: 경품 목록 조회 기능 구현

--- src/modules/draw/draw.controller.ts
+++ src/modules/draw/draw.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DrawService } from './draw.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Draw')
 @Controller('draw')
 export class DrawController {
     constructor(private readonly drawService: DrawService) {}
+
+    @Get()
+    @ApiOperation({
+        summary: '경품 목록 조회',
+        description:
+            '제공하는 경품 목록을 조회합니다. 총계가 적은 순으로 조회됩니다.',
+    })
+    async getAllDraws(): Promise<any> {
+        const draws = await this.drawService.getAllDraws();
+        return {
+            message: '경품 목록을 조회했습니다.',
+            data: draws,
+        };
+    }
 }
--- src/modules/draw/draw.service.ts
+++ src/modules/draw/draw.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { DrawRepository } from './repository/draw.repository';
+import { GetDrawResponse } from './dto/response/get.draw.response';
 
 @Injectable()
 export class DrawService {
     constructor(private readonly drawRepository: DrawRepository) {}
+
+    async getAllDraws(): Promise<GetDrawResponse[]> {
+        const draws = await this.drawRepository.getAllDraws();
+        return draws.map((draw) => new GetDrawResponse(draw));
+    }
 }
