03b1cb645498234a557ac1b3d6ec810406f2f86a
dlwhsk0 <hana.dev.314@gmail.com>
2024-12-05 02:58:48 +0900

    feat: 유저 검색 기능 구현

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
-export class UserRepository {}
+export class UserRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getUsers(userName: string): Promise<UserEntity[]> {
+        return this.prisma.user.findMany({
+            where: {
+                userName: {
+                    contains: userName, // 일부 문자열로 검색
+                    mode: 'insensitive', // 대소문자 구분 없이 검색
+                },
+            },
+            orderBy: {
+                userYear: 'asc', // 기수 기준 오름차순 정렬
+            },
+        });
+    }
+}
