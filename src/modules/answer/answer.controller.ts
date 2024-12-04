ed8ceb16e6347bf5e05b69020ba598df3dffee33
dlwhsk0 <hana.dev.314@gmail.com>
2024-12-05 03:11:55 +0900

    feat: 답변 개수 조회 기능 구현

--- src/modules/answer/answer.controller.ts
+++ src/modules/answer/answer.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AnswerService } from './answer.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Answer')
 @Controller('answer')
 export class AnswerController {
     constructor(private readonly answerService: AnswerService) {}
+
+    @Get('/count')
+    @ApiOperation({
+        summary: '답변 개수 조회',
+        description: '저장된 답변의 개수를 조회합니다.',
+    })
+    async getAnswerCount(): Promise<any> {
+        const answerCount = await this.answerService.getAnswerCount();
+        return {
+            message: '저장된 답변의 개수를 조회 했습니다.',
+            data: answerCount,
+        };
+    }
 }
--- src/modules/answer/repository/answer.repository.ts
+++ src/modules/answer/repository/answer.repository.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class AnswerRepository {}
+export class AnswerRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getAnswerCount(): Promise<number> {
+        return this.prisma.answer.count();
+    }
+}
