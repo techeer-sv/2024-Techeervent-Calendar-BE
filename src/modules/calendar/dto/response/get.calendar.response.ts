1115dcb1de28ba5700f895805aef54528367902b
dlwhsk0 <hana.dev.314@gmail.com>
2024-12-20 21:01:43 +0900

    refactor: 캘린더 조회 시의 response 내용 수정

--- src/modules/calendar/dto/response/get.calendar.response.ts
+++ src/modules/calendar/dto/response/get.calendar.response.ts
@@ -1,13 +1,11 @@
 import { CalendarEntity } from '../../entities/calendar.entity';
 
 export class GetCalendarResponse {
-    readonly calendarId: number;
     readonly calendarDate: number;
     readonly qa: { question: string; answer: string };
     readonly drawName?: string | null;
 
     constructor(calendarEntity: CalendarEntity) {
-        this.calendarId = calendarEntity.calendarId;
         this.calendarDate = calendarEntity.calendarDate;
         this.qa = {
             question: calendarEntity.question.questionContent,
